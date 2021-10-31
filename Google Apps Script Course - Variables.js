c43e3b471ed0650c4b0d9ff09a536de8e9df65e1
Aryan Irani <64119688+aryanirani123@users.noreply.github.com>
2021-10-31 14:22:19 +0530

    Variables_GAPS

---
+++ Google Apps Script Course - Variables.js
@@ -0,0 +1,39 @@
+const name = "Aryan Irani";
+
+function constfunction(){
+
+  const a = 10;
+  Logger.log(a)
+  console.log(a);
+
+}
+
+// let name = value
+
+function letfunction(){
+  
+  let b = 10;
+  Logger.log(b)
+  Logger.log(name)
+
+}
+
+// var name = value
+
+function varfunction(){
+
+  var c = 200;
+  console.log(c);
+  Logger.log(c)
+
+}
+
+function keypoints(){
+
+  const a = 10
+  Logger.log(a)
+
+  const y = 100;
+  Logger.log(y)
+
+}
