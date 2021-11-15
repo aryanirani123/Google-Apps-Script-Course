6a02043fe7713e4581f9915d91803d9843c52a3b
Aryan Irani <64119688+aryanirani123@users.noreply.github.com>
2021-11-15 19:03:09 +0530

    Create operators.js

---
+++ operators.js
@@ -0,0 +1,50 @@
+function mathoperators(){
+
+  const num1 = 10;
+  const num2 = 20;
+/*
+  Logger.log(num1 + num2);
+  Logger.log(num1 - num2);
+  Logger.log(num1 * num2);
+  Logger.log(num2/num1);
+*/
+
+  const fname = "Aryan";
+  const mname = "Romin";
+  const lname = "Irani";
+
+  Logger.log(fname + " "+ mname+" "+ lname)
+
+}
+
+function numbers(){
+
+  const num = 45;
+  const string = "hello";
+
+  Logger.log(typeof(num))
+  Logger.log(typeof(string))
+  
+
+}
+
+function operators(){
+
+
+  const num1 = 90;
+  const num2 = "90";
+
+  //Logger.log(typeof(num1));
+  //Logger.log(typeof(num2));
+
+  //Logger.log(num1 == num2)
+  //Logger.log(num1 === num2)
+
+
+  const number1 = 20
+  const number2 = 30
+
+  Logger.log(number1 > number2)
+  Logger.log(number1 < number2)
+
+}
